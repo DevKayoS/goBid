531cee5dba820572c951ff07e788332fdf7b1264
DevKayoS <kayoribeirodev@gmail.com>
2025-04-26 14:44:38 -0300

    feat: finalizando a criacao de usuarios

--- internal/api/user_handler.go
+++ internal/api/user_handler.go
@@ -1,13 +1,36 @@
 package api
 
 import (
+	"errors"
 	"net/http"
 
+	"github.com/DevKayoS/goBid/internal/services"
+	"github.com/DevKayoS/goBid/internal/useCase/user"
 	"github.com/DevKayoS/goBid/internal/utils"
 )
 
 func (api *Api) handleSignupUser(w http.ResponseWriter, r *http.Request) {
-	data, problems, err := utils.DecodeValidJson[CreateUserRequest]()
+	data, problems, err := utils.DecodeValidJson[user.CreateUserRequest](r)
+
+	if err != nil {
+		_ = utils.EncodeJson(w, r, http.StatusUnprocessableEntity, problems)
+		return
+	}
+
+	id, err := api.UserService.CreateUser(r.Context(), data.UserName, data.Email, data.Password, data.Bio)
+
+	if err != nil {
+		if errors.Is(err, services.ErrDuplicatedEmailOrPassword) {
+			_ = utils.EncodeJson(w, r, http.StatusUnprocessableEntity, map[string]any{
+				"error": "email or username already exists",
+			})
+			return
+		}
+	}
+
+	_ = utils.EncodeJson(w, r, http.StatusCreated, map[string]any{
+		"user_id": id,
+	})
 }
 
 func (api *Api) handleLoginUser(w http.ResponseWriter, r *http.Request) {
---
+++ internal/store/pgstore/errors_code.go
@@ -0,0 +1,3 @@
+package pgstore
+
+const UNIQUE_VALUE = "23505"
