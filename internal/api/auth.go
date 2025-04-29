c22fa4097968f34325ad74e4e5e713be930d20cc
DevKayoS <kayoribeirodev@gmail.com>
2025-04-28 23:23:28 -0300

    feat: config crsf token

--- internal/api/auth.go
+++ internal/api/auth.go
@@ -4,7 +4,16 @@ import (
 	"net/http"
 
 	"github.com/DevKayoS/goBid/internal/utils"
+	"github.com/gorilla/csrf"
 )
+
+func (api *Api) HandleGetCSRFtoken(w http.ResponseWriter, r *http.Request) {
+	token := csrf.Token(r)
+
+	utils.EncodeJson(w, r, http.StatusOK, map[string]any{
+		"csrf_token": token,
+	})
+}
 
 func (api *Api) AuthMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
